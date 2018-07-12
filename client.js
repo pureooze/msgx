e9aa8e5ef935fc5f7c4e13f2f4ff286c7a2287cb
Uzair Shamim <uzashamim@gmail.com>
2018-07-11 20:20:29 -0400

    Added API as an arg passed in by client to allow for testing

--- client.js
+++ client.js
@@ -1,21 +1,22 @@
-export default function (actions = {}, onDisconnect, debug = true) {
-  const port = chrome.runtime.connect()
-  let t = 0
-  const transactions = {}
+export default function(api, actions = {}, onDisconnect, debug = true) {
+  const port = api.runtime.connect();
+  let t = 0;
+  const transactions = {};
   port.onMessage.addListener(([t, actionOrResult, arg]) => {
     if (t > 0) {
-      if (debug) console.log(`rx:${t}`, actionOrResult)
-      transactions[t](actionOrResult)
+      if (debug) console.log(`rx:${t}`, actionOrResult);
+      transactions[t](actionOrResult);
     } else {
-      if (debug) console.log(`rx:${t}:${actionOrResult}`, arg)
-      actions[actionOrResult](arg)
+      if (debug) console.log(`rx:${t}:${actionOrResult}`, arg);
+      actions[actionOrResult](arg);
     }
-  })
-  onDisconnect && port.onDisconnect.addListener(onDisconnect)
-  return (action, arg) => new Promise(resolve => {
-    t++
-    if (debug) console.log(`tx:${t}:${action}`, arg)
-    port.postMessage([t, action, arg])
-    transactions[t] = resolve
-  })
+  });
+  onDisconnect && port.onDisconnect.addListener(onDisconnect);
+  return (action, arg) =>
+    new Promise(resolve => {
+      t++;
+      if (debug) console.log(`tx:${t}:${action}`, arg);
+      port.postMessage([t, action, arg]);
+      transactions[t] = resolve;
+    });
 }
--- server.js
+++ server.js
@@ -1,18 +1,25 @@
-export default function (actions = {}, onConnect, onDisconnect, debug = true) {
-  chrome.runtime.onConnect.addListener(port => {
-    if (debug) console.log('connect', port.sender)
-    const data = {}
+export default function(
+  api,
+  actions = {},
+  onConnect,
+  onDisconnect,
+  debug = true
+) {
+  api.runtime.onConnect.addListener(port => {
+    if (debug) console.log("connect", port.sender);
+    const data = {};
     port.onMessage.addListener(async ([t, action, arg]) => {
-      if (debug) console.log(`rx:${t}:${action}`, arg)
-      const reply = await actions[action](arg, port.sender, data)
-      port.postMessage([ t, reply ])
-      if (debug) console.log(`tx:${t}`, reply)
-    })
+      if (debug) console.log(`rx:${t}:${action}`, arg);
+      const reply = await actions[action](arg, port.sender, data);
+      port.postMessage([t, reply]);
+      if (debug) console.log(`tx:${t}`, reply);
+    });
     const msg = (action, arg) => {
-      if (debug) console.log(`tx:${0}:${action}`, arg)
-      port.postMessage([0, action, arg])
-    }
-    onConnect && onConnect(port.sender, msg, data)
-    onDisconnect && port.onDisconnect.addListener(() => onDisconnect(port.sender, data))
-  })
+      if (debug) console.log(`tx:${0}:${action}`, arg);
+      port.postMessage([0, action, arg]);
+    };
+    onConnect && onConnect(port.sender, msg, data);
+    onDisconnect &&
+      port.onDisconnect.addListener(() => onDisconnect(port.sender, data));
+  });
 }
